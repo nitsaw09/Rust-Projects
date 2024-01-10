17b6d91257dcf3eb88c796f3452486ae5f386973
nitsaw09 <niteshghuge09@gmail.com>
2024-01-10 18:24:35 +0530

    rust functions

--- functions/target/debug/deps/functions-3d0ff0758983089f.d
+++
@@ -1,5 +0,0 @@
-/home/nitsaw/Rust-Projects/functions/target/debug/deps/functions-3d0ff0758983089f: src/main.rs
-
-/home/nitsaw/Rust-Projects/functions/target/debug/deps/functions-3d0ff0758983089f.d: src/main.rs
-
-src/main.rs:
--- functions/target/debug/functions.d
+++
@@ -1,1 +0,0 @@
-/home/nitsaw/Rust-Projects/functions/target/debug/functions: /home/nitsaw/Rust-Projects/functions/src/main.rs
