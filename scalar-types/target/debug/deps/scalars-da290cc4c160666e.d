9150ca0e9b0a72079412d9183abd9171a436ea00
nitsaw09 <niteshghuge09@gmail.com>
2024-01-10 17:56:45 +0530

    compound types

--- scalar-types/target/debug/deps/scalars-da290cc4c160666e.d
+++
@@ -1,5 +0,0 @@
-/home/nitsaw/rust-program/scalar-types/target/debug/deps/scalars-da290cc4c160666e: src/main.rs
-
-/home/nitsaw/rust-program/scalar-types/target/debug/deps/scalars-da290cc4c160666e.d: src/main.rs
-
-src/main.rs:
--- scalar-types/target/debug/scalars.d
+++
@@ -1,1 +0,0 @@
-/home/nitsaw/rust-program/scalar-types/target/debug/scalars: /home/nitsaw/rust-program/scalar-types/src/main.rs
